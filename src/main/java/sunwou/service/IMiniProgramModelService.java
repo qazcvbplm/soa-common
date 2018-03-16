6cf8323bd45358ff0ecdbc0636a7b5cd6f4e001b
hanyangji <893891013@qq.com>
2018-03-16 13:24:55 +0800

    201803161324

--- src/main/java/sunwou/service/IMiniProgramModelService.java
+++ src/main/java/sunwou/service/IMiniProgramModelService.java
@@ -1,6 +1,46 @@
 package sunwou.service;
 
+import java.util.List;
+
+import sunwou.entity.MiniProgramModel;
+import sunwou.mongo.util.QueryObject;
+
 public interface IMiniProgramModelService {
 
+	/**
+	 * 查询小程序模板列表
+	 * @author hava
+	 * @param qo
+	 * @return 小程序模板列表
+	 * 2018年3月16日上午11:03:58
+	 */
+	public List<MiniProgramModel> miniProList(QueryObject qo);
+	
+	/**
+	 * 根据id查询
+	 * @author hava
+	 * @param id
+	 * @return 小程序模板实体类
+	 * 2018年3月16日上午11:05:54
+	 */
+	public MiniProgramModel findById(String id);
+	
+	/**
+	 * 添加一个小程序模板
+	 * @author hava
+	 * @param miniProModel
+	 * @return 小程序模板id
+	 * 2018年3月16日下午12:28:05
+	 */
+	public String addMiniProModel(MiniProgramModel miniProModel);
+	
+	/**
+	 * 更新小程序模板信息
+	 * @author hava
+	 * @param miniProModel
+	 * @return  更新数量
+	 * 2018年3月16日下午12:29:12
+	 */
+	public int update(MiniProgramModel miniProModel);
 	
 }
--- src/main/java/sunwou/service/IMiniProgramService.java
+++ src/main/java/sunwou/service/IMiniProgramService.java
@@ -8,10 +8,39 @@ import sunwou.mongo.util.QueryObject;
 public interface IMiniProgramService {
 
 	/**
-	 * 
-	 * hava
-	 * 2018年3月16日
-	 * 上午10:10:53
+	 * 查询小程序列表
+	 * @author hava
+	 * @param qo
+	 * @return 小程序列表
+	 * 2018年3月16日上午10:44:50
 	 */
-	public List<MiniProgram> find(QueryObject qo);
+	public List<MiniProgram> miniProgramList(QueryObject qo);
+	
+	/**
+	 * 根据id查询
+	 * @author hava
+	 * @param id
+	 * @return 小程序实体类
+	 * 2018年3月16日上午10:46:34
+	 */
+	public MiniProgram findById(String id);
+	
+	/**
+	 * 添加一个小程序
+	 * @author hava
+	 * @param miniProgram 
+	 * @return 小程序id
+	 * 2018年3月16日上午10:51:19
+	 */
+	public String addMiniPro(MiniProgram miniProgram);
+	
+	/**
+	 * 更新小程序信息
+	 * @author hava
+	 * @param miniProgram
+	 * @return 更新数量
+	 * 2018年3月16日上午10:52:50
+	 */
+	public int update(MiniProgram miniProgram);
+	
 }

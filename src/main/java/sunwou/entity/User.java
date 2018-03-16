4eaefefc1039249ec5e79a988951c713f6911085
tzh <471594060@qq.com>
2018-03-16 10:33:03 +0800

    201803161033

--- src/main/java/sunwou/entity/User.java
+++ src/main/java/sunwou/entity/User.java
@@ -23,8 +23,57 @@ public class User extends MongoBaseEntity implements Serializable{
 	private String phone;
 	//用户密码
 	private String passWord;
+	//用户省份
+    private String province;	
+	//用户城市
+    private String city;
+	//用户昵称
+	private String nickName;
+	//用户头像
+	private String avatarUrl;
+	//用户性别
+	private String gender;
 	
 	
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getAvatarUrl() {
+		return avatarUrl;
+	}
+
+	public void setAvatarUrl(String avatarUrl) {
+		this.avatarUrl = avatarUrl;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 
 	public String getPassWord() {
 		return passWord;
